ce7291fc3cbda3f4e2a5f9b3b4079de1f025d89f
Monika-feg <mala88@hotmail.comcd>
2025-10-15 12:25:31 +0200

    nu kan man lägga till en tom kundvagn

--- src/main/java/com/kawaiicanvas/kawaicanvas/Cart/CartController.java
+++ src/main/java/com/kawaiicanvas/kawaicanvas/Cart/CartController.java
@@ -1,5 +1,21 @@
 package com.kawaiicanvas.kawaicanvas.Cart;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/cart")
 public class CartController {
+
+    @Autowired
+    private CartService cartService;
+
+    @PostMapping()
+    public Cart createNewCart(@RequestBody Cart cart) {
+        return cartService.createNewCart(cart);
+    }
 
 }
