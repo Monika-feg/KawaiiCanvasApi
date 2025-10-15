b111d577876eabe7b67ca474922d5b09a485dc42
Monika-feg <mala88@hotmail.comcd>
2025-10-15 14:10:55 +0200

    nu kan jag lägga till en tavla i min varukorg, kommer nog ändra den lite sen när jag ska lägga in mer felhantering

--- src/main/java/com/kawaiicanvas/kawaicanvas/Cart/CartService.java
+++ src/main/java/com/kawaiicanvas/kawaicanvas/Cart/CartService.java
@@ -1,7 +1,12 @@
 package com.kawaiicanvas.kawaicanvas.Cart;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.kawaiicanvas.kawaicanvas.Canvas.Canvas;
+import com.kawaiicanvas.kawaicanvas.Canvas.CanvasRepository;
 
 // felhantering kommer inom kort
 @Service
@@ -9,6 +14,8 @@ public class CartService {
 
     @Autowired
     private CartRepository cartRepository;
+    @Autowired
+    private CanvasRepository canvasRepository;
 
     public Cart createNewCart(Cart cart) {
         return cartRepository.save(cart);
@@ -16,6 +23,21 @@ public class CartService {
 
     public Cart getCartById(String id) {
         return cartRepository.findById(id).orElse(null);
+    }
+
+    public Cart addCanvasToCart(String cartId, String canvasId) {
+        Cart cart = getCartById(cartId);
+        Canvas canvas = canvasRepository.findById(canvasId).orElse(null);
+
+        boolean isCanvasInCart = cart.getCanvases().stream()
+                .anyMatch(existingCanvas -> existingCanvas.getId().equals(canvasId));
+
+        if (!isCanvasInCart) {
+            cart.getCanvases().add(canvas);
+            return cartRepository.save(cart);
+        }
+
+        return cart;
     }
 
 }
