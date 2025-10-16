e6b05c60db2e650f97cbe0f622725ea7393faf71
Monika-feg <mala88@hotmail.comcd>
2025-10-16 09:05:50 +0200

    kan nu hämta order med hjälp av id

--- src/main/java/com/kawaiicanvas/kawaicanvas/Order/OrderController.java
+++ src/main/java/com/kawaiicanvas/kawaicanvas/Order/OrderController.java
@@ -1,6 +1,7 @@
 package com.kawaiicanvas.kawaicanvas.Order;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,9 +15,16 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
+    // lägg till ny order
     @PostMapping("/{cartId}")
     public Order createOrder(@RequestBody Order order, @PathVariable String cartId) {
         return orderService.saveNewOrder(order, cartId);
+    }
+
+    // hämta order med hjälp av id
+    @GetMapping("/{id}")
+    public Order getOrderById(@PathVariable String id) {
+        return orderService.getOrderById(id);
     }
 
 }
